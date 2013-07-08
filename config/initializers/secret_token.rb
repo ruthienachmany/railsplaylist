15c06b290cd2144ab12b876232faf4b5af0d05ce
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-07-08 10:40:26 -0400

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPlaylist::Application.config.secret_key_base = '6a12faccdc4df7e8626f2cc2ce4a691929b10f5d7f9693c1bd1213535db5a6c1a827d93340d5bc36fd0ddea969968b28dbf8f65225a78945c5f7fd40a5d4a5ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPlaylist::Application.config.session_store :cookie_store, key: '_rails-playlist_session'
